43dd47495d99e436a215257af51a2b79b3435211
valera arnovich <61805285+valera1w21@users.noreply.github.com>
2026-06-10 21:18:59 +0300

    Add hardcoded SECRET_KEY for development
    Added a hardcoded secret key for development purposes.

--- backend/To_Do/settings.py
+++ backend/To_Do/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-f0aabsypx%ueye0zkln-t=!cwr94lth)2aa7)()!zllsiq0kf0'
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
