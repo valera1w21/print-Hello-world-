9f5c6d52124ad351b324f47c692e4ca14d75d2a9
valera arnovich <61805285+valera1w21@users.noreply.github.com>
2026-06-10 20:53:01 +0300

    Refactor database settings to use dj_database_url
    Updated database configuration to use dj_database_url for better flexibility.

--- backend/To_Do/settings.py
+++ backend/To_Do/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import pymysql
 pymysql.install_as_MySQLdb()
+import os
+import dj_database_url
 
 from pathlib import Path
 from decouple import config
@@ -78,10 +80,10 @@ WSGI_APPLICATION = 'To_Do.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 
