c2e8fa3403f1aeb60521c07c29e4eef85c006c28
valera arnovich <61805285+valera1w21@users.noreply.github.com>
2026-06-10 20:21:54 +0300

    Add completed field to Task model

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
